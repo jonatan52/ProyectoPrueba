2b74184b1106209b102edd48d79b63b2ae0ad1df
Jonatan Tarriba <tarriba52@gmail.com>
2022-11-25 14:06:35 -0500

    Archivos Backend y Frontend

---
+++ VentasCamistas/src/main/java/com/example/demo/Controllers/HolaController.java
@@ -0,0 +1,15 @@
+
+package com.example.demo.Controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HolaController {
+    
+    @GetMapping("/")
+    public String hola(){
+        return "Hola mundo!!";
+    }
+    
+}
